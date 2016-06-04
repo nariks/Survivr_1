f4ed8c73fd06b6e9a91030620c42b0b47e38607d
nariks <happycow@gmail.com>
2016-06-04 17:42:11 +1000

    Added colors.rb file and colorizr.rb with colors method

---
+++ lib/colorizr.rb
@@ -0,0 +1,8 @@
+class String 
+  def self.colors 
+    colors = [:red, :green, :yellow, :blue, :pink, :light_blue, :white, :light_grey, :black]
+  end
+end
+
+  def self.create_colors
+    class_eval("def 
